1a7025a35e168eee100665fba8f207b9708875a5
root <root@DESKTOP-PCS759L.localdomain>
2020-12-03 15:50:15 +0530

    modified one file

---
+++ bangalore/src/main/java/santosh/App.java
@@ -0,0 +1,13 @@
+package sanotsh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
